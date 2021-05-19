6486b2811a3e3655cdca7c3c9e5e711b420007e1
teodoraruvceski <teodoraruvceski@gmail.com>
2021-05-19 23:34:12 +0200

    Konekcija servera

--- ProjektniZadatak1_LoadBalancer/LoadBalancer/Program.cs
+++ ProjektniZadatak1_LoadBalancer/LoadBalancer/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,17 @@ namespace LoadBalancer
 {
     class Program
     {
-        static void Main(string[] args)
+		public static void Conn()
+		{
+			ServiceHost sh = new ServiceHost(typeof(BalancerService));
+			sh.AddServiceEndpoint(typeof(Common.IBalancerService), new NetTcpBinding(), new Uri("net.tcp://localhost:4002/IBalancerService"));
+			sh.Open();
+			Console.WriteLine("Servis otvoren.");
+
+			Console.Read();
+			sh.Close();
+		}
+		static void Main(string[] args)
         {
         }
     }
